ad10465692fd2dcd2b6f10670de65822fdff88d4
William Chiu <william.chiu16@trinityschoolnyc.org>
2015-07-16 12:51:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ var realcounter;
 var resetcounter = 0;
 var backgroundColor;
 
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 function setup() {
   counter = createP();
   var cnv = createCanvas(660, 660);
